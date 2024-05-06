619610401b75953ec1be7d7795ecebfe6b73dc74
Fibinse Xavier <fibinse.xavier-e@maf.ae>
2024-05-06 18:25:11 +0530

    chore: added code to get one metric using octis

--- run-octis-benchmark.py
+++ run-octis-benchmark.py
@@ -3,6 +3,8 @@ import string
 from pathlib import Path
 
 from octis.preprocessing.preprocessing import Preprocessing
+from octis.dataset.dataset import Dataset
+from octis.models.LDA import LDA
 os.chdir(os.path.pardir)
 
 target_folder = Path('/Users/fibinse/testing_ground/topic-modelling-benchmarks/Octis-data-prep/survey-response-data')
@@ -16,4 +18,16 @@ dataset = preprocessor.preprocess_dataset(documents_path=str(target_folder.joinp
                                           labels_path=str(target_folder.joinpath('labels.txt')))
 
 # save the preprocessed dataset
-dataset.save('survey_response')
+dataset.save('octis-benchmark-survey_response')
+
+dataset = Dataset()
+dataset.load_custom_dataset_from_folder('octis-benchmark-survey_response')
+
+model = LDA(num_topics=25)  # Create model
+model_output = model.train_model(dataset) # Train the model
+
+from octis.evaluation_metrics.diversity_metrics import TopicDiversity
+
+metric = TopicDiversity(topk=10) # Initialize metric
+topic_diversity_score = metric.score(model_output) # Compute score of the metric
+import pdb; pdb.set_trace()
